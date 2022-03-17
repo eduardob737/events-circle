5c2e750181d15319faaa5e0ca4a2c19d2b200da5
Pedro Henrique de Souza Araujo <pearaujo@server01.foursys.local>
2022-03-17 15:29:17 -0300

    Adicionando dependencias

---
+++ app/src/main/java/com/example/projetoeventos/retrofit/RetrofitMethods.java
@@ -0,0 +1,15 @@
+package com.example.projetoeventos.retrofit;
+
+import com.example.fourpay.model.Conta;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface RetrofitMethods {
+
+}
+
+
