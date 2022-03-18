01aec3a298d9a0343f5ca58152b8327d609b7adb
Pedro Henrique de Souza Araujo <pearaujo@server01.foursys.local>
2022-03-18 15:12:35 -0300

    Adicionando métodos do Retrofit

--- app/src/main/java/com/example/projetoeventos/retrofit/RetrofitMethods.java
+++ app/src/main/java/com/example/projetoeventos/retrofit/RetrofitMethods.java
@@ -1,14 +1,34 @@
 package com.example.projetoeventos.retrofit;
 
-import com.example.fourpay.model.Conta;
+
+import com.example.projetoeventos.model.Usuario;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface RetrofitMethods {
+
+    @POST("usuarios/criar")
+    Call<Usuario> usuarioPost(@Body Usuario user);
+
+    @GET("usuarios/{id}")
+    Call<Usuario> usuarioGet(@Path("id") Long id);
+
+    @GET("usuarios/nome/{nome}")
+    Call<Usuario> usuarioGetByNome(@Path("nome") String nome);
+
+    @GET("usuarios/cpf/{cpf}")
+    Call<Usuario> usuarioGetByCpf(@Path("cpf") String cpf);
+
+    @DELETE("usuarios/delete")
+    Call<Usuario> usuarioDelete(@Body Usuario user);
+
+    @GET("usuarios/updade")
+    Call<Usuario> usuarioUpdate(@Body Usuario update);
 
 }
 
